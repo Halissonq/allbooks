de496bbfc8d999d98a01e486d7ef0fd7abe1b5c9
Halissonq <130264769+Halissonq@users.noreply.github.com>
2024-11-19 16:54:06 -0300

    altera server

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ function createToken(payload, expiresIn = '12h') {
   return jwt.sign(payload, SECRET_KEY, { expiresIn })
 }
 
+
+
+
+
+
 function verifyToken(token) {
   return jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ? decode : err)
 }
